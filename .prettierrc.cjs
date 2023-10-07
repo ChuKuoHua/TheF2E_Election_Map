823550446d4aa1a55733debe6269cfc50fd6e2d6
Yilee <yileefrontend@gmail.com>
2023-10-07 19:00:25 +0800

    加入format指令與新增條件

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-  printWidth: 100,          // 單行長度
-  tabWidth: 2,              // Tab 的空格數量
-  semi: false,              // 結尾使否需要分號
-  singleQuote: true,        // 字串使用單引號
-  trailingComma: 'none',     // 如 Object、Array 內的元素不需要尾隨逗號
-  formatOnSave: true
+  printWidth: 100, // 單行長度
+  tabWidth: 2, // Tab 的空格數量
+  semi: false, // 結尾使否需要分號
+  singleQuote: true, // 字串使用單引號
+  trailingComma: 'none', // 如 Object、Array 內的元素不需要尾隨逗號
+  formatOnSave: true,
+  endOfLine: 'auto'
 }
