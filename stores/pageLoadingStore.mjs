15e54dfe766cdd3ac0dc00621772f1f96fd65db5
Yilee <yileefrontend@gmail.com>
2023-11-04 22:27:53 +0800

    [新增] loading：組件 & pinia

---
+++ stores/pageLoadingStore.mjs
@@ -0,0 +1,28 @@
+export const usePageLoadingStore = defineStore('pageLoading', () => {
+  const loading = ref(false)
+  let loadingStartTime = null
+
+  const changeLoadingStatus = (status) => {
+    if (status) {
+      loadingStartTime = Date.now() // 計時
+      loading.value = true
+    } else {
+      const elapsedTime = Date.now() - (loadingStartTime || 0)
+      const minimumDelay = 3000 // 最少維持秒數
+
+      if (elapsedTime < minimumDelay) {
+        //未滿秒數，延遲處理
+        const remainingTime = minimumDelay - elapsedTime
+
+        setTimeout(() => {
+          console.log('remainingTime', remainingTime)
+          loading.value = false
+        }, remainingTime)
+      } else {
+        loading.value = false
+      }
+    }
+  }
+
+  return { loading, changeLoadingStatus }
+})
